8b3bc9e21c1c3523c67c547016077f83d4a1184c
Charles Liang <charles.liang@pushpay.com>
2023-12-13 20:08:59 +1300

    day 4, part 1

---
+++ 2023/Day_04/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var useSample = false;
+var file = System.IO.File.ReadAllLines(useSample ? "sample_input.txt" : "input.txt");
+
+var cardCollection = new List<Card>();
+foreach (var line in file)
+{
+    cardCollection.Add(new Card(line));
+}
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(cardCollection.Sum(x => x.Points()));
+
+public class Card
+{
+    public Card(string line)
+    {
+        var segments = line.Split(':');
+        CardNumber = int.Parse(segments[0].Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]);
+        Numbers = segments[1].Split('|')[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+            .ToList();
+        WinningNumbers = segments[1].Split('|')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+            .ToList();
+    }
+
+    public int CardNumber { get; set; }
+    public List<int> Numbers { get; set; }
+    public List<int> WinningNumbers { get; set; }
+
+    public int Points()
+    {
+        var nums = Numbers.Intersect(WinningNumbers);
+
+        if (nums.Any() == false)
+        {
+            return 0;
+        }
+
+        var points = 1;
+        for (var i = 1; i < nums.Count(); i++)
+        {
+            points = points * 2;
+        }
+
+        return points;
+    }
+}
