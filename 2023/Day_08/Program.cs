11d79a3d3383ec8d72df5eb97b9a376871b4df13
Charles Liang <charles.liang@pushpay.com>
2023-12-20 16:07:20 +1300

    day 8; part 2

--- 2023/Day_08/Program.cs
+++ 2023/Day_08/Program.cs
@@ -1,45 +1,76 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections.Concurrent;
+
 var useSample = false;
-var file = File.ReadAllLines(useSample ? "sample_input2.txt" : "input.txt");
+var file = File.ReadAllLines(useSample ? "sample_input3.txt" : "input.txt");
 
+var network = ParseNetwork(file);
 var directionList = file[0];
-var network = new Dictionary<string, string[]>();
-foreach (var line in file.Skip(2))
+
+var paths = GetStartingPaths(network);
+var distances = new Dictionary<string, int>();
+
+foreach (var start in paths)
 {
-    var segment = line.Split('=');
-    network.Add(
-        segment[0].Trim(),
-        segment[1]
-            .Split(',')
-            .Select(x => new string(x.Where(char.IsLetter).ToArray()))
-            .ToArray());
+    distances.Add(start, GetTraversalDistance(start));
 }
 
-var i = 0;
-var currentNode = "AAA";
-while (true)
-{
-    if (currentNode == "ZZZ")
-    {
-        break;
-    }
-    
-    var node = network[currentNode];
-    string? nextNode = "";
-    switch (directionList[i % directionList.Length])
-    {
-        case 'L' :
-            nextNode = node[0];
-            break;
-        case 'R':
-            nextNode = node[1];
-            break;
-    }
+Console.WriteLine($"Hello, World! part 2: {lcm(distances.Select(x => (long)x.Value).ToList())}");
+return;
+//8245452805243
 
-    currentNode = nextNode;
-    i++;
+static long gcd(long n1, long n2)
+{
+    if (n2 == 0) return n1;
+
+    return gcd(n2, n1 % n2);
 }
 
-Console.WriteLine($"current node: {currentNode}, steps: {i}");
-Console.WriteLine("Hello, World!");
+static long lcm(List<long> numbers)
+{
+    return numbers.Aggregate((s, val) => s * val / gcd(s, val));
+}
+
+int GetTraversalDistance(string start)
+{
+    var i = 0;
+    var currentNode = start;
+    while (currentNode.EndsWith('Z') == false)
+    {
+        var node = network[currentNode];
+        currentNode = GetNextDirection(i, directionList) switch
+        {
+            'L' => node[0],
+            'R' => node[1],
+            _ => throw new NotSupportedException()
+        };
+        i++;
+    }
+
+    return i;
+}
+
+char GetNextDirection(int x, string l) => l[x % l.Length];
+
+Dictionary<string, string[]> ParseNetwork(string[] file)
+{
+    var network = new Dictionary<string, string[]>();
+    foreach (var line in file.Skip(2))
+    {
+        var segment = line.Split('=');
+        network.Add(
+            segment[0].Trim(),
+            segment[1]
+                .Split(',')
+                .Select(x => new string(x.Where(char.IsLetterOrDigit).ToArray()))
+                .ToArray());
+    }
+
+    return network;
+}
+
+List<string> GetStartingPaths(Dictionary<string, string[]> network)
+{
+    return network.Keys.Where(k => k.EndsWith('A')).ToList();
+}
